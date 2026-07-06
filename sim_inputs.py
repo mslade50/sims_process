cf25917e8dd5719afee79f1048211e8924feffbe
McKinley Slade <mckinleyslade@gmail.com>
2026-07-06 15:36:27 -0400

    Update dashboard data for Render deploy

--- sim_inputs.py
+++ sim_inputs.py
@@ -4,7 +4,7 @@ import numpy as np
 ##New sim inputs
 SIMULATIONS   = 100000
 STD_DEV       = 2.8
-PAR           = 71
+PAR           = 70
 CUT_LINE      = 65
 USE_10_SHOT_RULE = False
 WIND_FACTOR_SIM  = 0.12  # must match your main script
@@ -24,10 +24,10 @@ wind_speed_base=12.2
 
 start_yr=2019 #first year of data you want to consider in your course baslines
 tour='pga'
-event_ids = [30]
-course_id = 669
-tourney = 'deere'
-course_par = 71
+event_ids = [541]
+course_id = 977
+tourney = 'scottish'
+course_par = 70
 course_name = "" #this is for the multi course showdown sims to id proper course
 # course_name = "Arnold Palmer's Bay Hill Club & Lodge"
 
@@ -35,7 +35,7 @@ major_adjustment = 0.0022 if any(eid in [33, 14, 100, 26] for eid in event_ids) 
 links_adjustment = 1 if any(eid in [100,541] for eid in event_ids) else 0
 
 #for multiple course setups in the showdown sim
-course_id_1=935
+course_id_1=977
 course_id_2=0
 
 #cut rules. Line is inclusive of ties, shot rule should be 0 as a default
